a4942acae92a9fc2a97c55ac1870760f136a0719
Davies Precious <preshy.d5@gmail.com>
2023-11-30 00:18:26 +0100

    warning modal

--- components/Oops.js
+++ components/Oops.js
@@ -1,18 +1,27 @@
 const Oops = () => {
   return (
-    <div className="font-playfair">
-      <div>
+    <div>
+      <div className="font-playfair bg-dime h-screen flex items-center justify-center text-center">
         <div>
-          <h1 className="font-bebas text-red text-[25px]">Oops!!</h1>
-        </div>
-        <div>
-          <p>
-            Your comment has been flagged for inappropriate language. Let's keep
-            it positive. Second warning!!—watch your language
-          </p>
-        </div>
-        <div>
-          <h1 className="font-bebas text-[25px]">Appeal Option</h1>
+          <div className="pb-3">
+            <h1 className="font-bebas text-red text-[1.875rem]">Oops!!</h1>
+          </div>
+          <div className="px-[15%]">
+            <p className="text-[1.125rem] py-1">
+              Your comment has been flagged for inappropriate language. Let's
+              keep it positive.
+            </p>
+          </div>
+          <div className="pb-6">
+            <p className="text-[1.125rem] py-1">
+              Second warning!!—watch your language
+            </p>
+          </div>
+          <div>
+            <button className="text-[1.1875rem] font-semibold w-[26rem] rounded-[0.5rem] text-white bg-red py-[1.125rem] px-[9rem]">
+              Appeal
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- components/Suspended.js
+++ components/Suspended.js
@@ -1,15 +1,22 @@
 const Suspended = () => {
   return (
-    <div className="font-playfair">
+    <div className="font-playfair bg-dime h-screen flex items-center justify-center text-center">
       <div>
-        <div>
-          <p>Account temporarily suspended due to repeated violations. </p>
+        <div className="pb-3">
+          <h1 className="font-bebas text-[1.875rem] text-red">NOTE</h1>
         </div>
         <div>
-          <p>We prioritize a safe and respectful space. </p>
+          <p className="text-[1.125rem] py-1">
+            Your Account has been suspended due to repeated violations.
+          </p>
         </div>
         <div>
-          <p>Contact support for concerns.</p>
+          <p className="text-[1.125rem] py-1">
+            We prioritize a safe and respectful space.
+          </p>
+        </div>
+        <div>
+          <p className="text-[1.125rem] py-1">Contact support for concerns.</p>
         </div>
       </div>
     </div>

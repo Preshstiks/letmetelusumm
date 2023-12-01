b3135bf6e2799ffacfbeb64b627208c47da5f826
Davies Precious <preshy.d5@gmail.com>
2023-12-01 20:49:23 +0100

    content added

--- components/Suspended.js
+++ components/Suspended.js
@@ -2,8 +2,8 @@ const Suspended = () => {
   return (
     <div className="font-playfair bg-dime h-screen flex items-center justify-center text-center">
       <div>
-        <div className="mx-auto border flex justify-center items-center border-red py-[0.5rem] px-[0.69rem] w-[6.5rem] h-[3.25rem] rounded-[0.25rem]">
-          <h1 className="font-bebas text-[1.875rem] text-red">NOTE</h1>
+        <div className="mx-auto border flex justify-center items-center mb-2 border-red py-[0.5rem] px-[0.69rem] w-[6.5rem] h-[3.25rem] rounded-[0.25rem]">
+          <h1 className="font-bebas text-[1.875rem] text-red">BLOCKED</h1>
         </div>
         <div className="py-1">
           <p className="text-[1.125rem]">
